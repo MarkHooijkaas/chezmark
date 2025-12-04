f010b0560ef84a90295effcac794dad50e2e2846
Mark Hooijkaas <mark@hooijkaas.net>
2025-12-05 00:37:00 +0100

    fix socket export

--- bash/bashrc.d/ssh-agent.bash
+++ bash/bashrc.d/ssh-agent.bash
@@ -1,5 +1,5 @@
-if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-  eval `ssh-agent`
-  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-  export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+export SSH_AUTH_SOCK=${SSH_AUTH_SOCK:-~/.ssh/ssh_auth_sock}
+if [[ ! -S $SSH_AUTH_SOCK ]]; then
+  eval `ssh-agent -a $SSH_AUTH_SOCK`
+  #ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
 fi
