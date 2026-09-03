10fa597db3f9305cb89f594ce03776f89349791e
Mark Hooijkaas <mark@hooijkaas.net>
2026-09-03 13:35:43 +0200

    refactor of wezterm lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,8 +1,52 @@
 local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local act = wezterm.action
+local config = wezterm.config_builder()
 
-local config = {}
+-- see: https://fredrikaverpil.github.io/blog/2024/10/20/session-management-in-wezterm-without-tmux/
+wezterm.on("update-right-status", function(window, pane)
+  window:set_right_status(window:active_workspace())
+end)
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+
+-- keymaps
+local keys={}
+table.insert(keys, { key = "s", mods = "CTRL|SHIFT", action = workspace_switcher.switch_workspace() })
+table.insert(keys, { key = "y", mods = "CTRL|SHIFT", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) })
+-- table.insert(keys, { key = "[", mods = "CTRL|SHIFT", action = act.SwitchWorkspaceRelative(1) })
+-- table.insert(keys, { key = "]", mods = "CTRL|SHIFT", action = act.SwitchWorkspaceRelative(-1) })
+table.insert(keys, { key = "k", mods = "CTRL|SHIFT", action = act.SwitchToWorkspace({ name='~/work/kisst/karmah'}) })
+table.insert(keys, { key = 'n', mods = 'CTRL', action = act.SwitchWorkspaceRelative(1) })
+table.insert(keys, { key = 'p', mods = 'CTRL', action = act.SwitchWorkspaceRelative(-1) })
+
+config.font_size = 20.0
+config.enable_scroll_bar = true
+-- config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = 'CGA'
+
+config.window_frame = {
+  font_size = 18.0,
+}
 
 -- Use Fish as the default shell in login mode
-config.default_prog = { '/home/linuxbrew/.linuxbrew/bin/fish', '-l' }
+if string.find(wezterm.target_triple, 'darwin') then
+  -- config.font_size = 14.0
+  -- config.window_background_opacity = 0.85
+  config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+  workspace_switcher.zoxide_path = "/opt/homebrew/bin/zoxide"
 
+-- Check for Linux
+elseif string.find(wezterm.target_triple, 'linux') then
+  -- config.font_size = 11.0
+  -- config.window_background_opacity = 1.0
+  config.default_prog = { '/home/linuxbrew/.linuxbrew/bin/fish', '-l' }
+  workspace_switcher.zoxide_path = "/home/linuxbrew/.linuxbrew/bin/zoxide"
+end
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+config.keys = keys
 return config
